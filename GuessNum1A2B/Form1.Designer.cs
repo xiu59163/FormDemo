f3df44d00784737c343b69ca4ee466abc6d91b5e
You <azsx59163@gmail.com>
2026-05-26 14:34:18 +0800

    1A2B

--- GuessNum1A2B/Form1.Designer.cs
+++ GuessNum1A2B/Form1.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnStart = new Button();
+            btnAnswer = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            btnCheck = new Button();
+            btnRestart = new Button();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(37, 23);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(80, 35);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "開始遊戲";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnAnswer
+            // 
+            btnAnswer.Location = new Point(37, 110);
+            btnAnswer.Name = "btnAnswer";
+            btnAnswer.Size = new Size(80, 35);
+            btnAnswer.TabIndex = 1;
+            btnAnswer.Text = "看答案";
+            btnAnswer.UseVisualStyleBackColor = true;
+            btnAnswer.Click += btnAnswer_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 283);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 2;
+            label1.Text = "輸入";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(53, 311);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(218, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // btnCheck
+            // 
+            btnCheck.Location = new Point(53, 356);
+            btnCheck.Name = "btnCheck";
+            btnCheck.Size = new Size(80, 35);
+            btnCheck.TabIndex = 4;
+            btnCheck.Text = "檢查答案";
+            btnCheck.UseVisualStyleBackColor = true;
+            btnCheck.Click += btnCheck_Click;
+            // 
+            // btnRestart
+            // 
+            btnRestart.Location = new Point(191, 356);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(80, 35);
+            btnRestart.TabIndex = 5;
+            btnRestart.Text = "放棄重來";
+            btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Click += btnRestart_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(378, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 6;
+            label2.Text = "遊戲歷程";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(378, 42);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(294, 379);
+            listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(btnRestart);
+            Controls.Add(btnCheck);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(btnAnswer);
+            Controls.Add(btnStart);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnStart;
+        private Button btnAnswer;
+        private Label label1;
+        private TextBox textBox1;
+        private Button btnCheck;
+        private Button btnRestart;
+        private Label label2;
+        private ListBox listBox1;
     }
 }
--- GuessNum1A2B/Form1.cs
+++ GuessNum1A2B/Form1.cs
@@ -2,9 +2,105 @@ namespace GuessNum1A2B
 {
     public partial class Form1 : Form
     {
+        string answer = "";
+        Random rnd = new Random();
+
+
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+        private void StartGame()
+        {
+            answer = GenerateAnswer();
+
+            textBox1.Clear();
+            listBox1.Items.Clear();
+
+            textBox1.Enabled = true;
+            btnCheck.Enabled = true;
+            btnAnswer.Enabled = true;
+            btnRestart.Enabled = true;
+
+            textBox1.Focus();
+        }
+        private string GenerateAnswer()
+        {
+            //產生answer
+            return string.Join("", Enumerable.Range(0, 10)
+                .OrderBy(x => rnd.Next())
+                .Take(4));
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string guess = textBox1.Text;
+
+            if (!IsValidGuess(guess))
+            {
+                MessageBox.Show("請輸入 4 個不重複的數字");
+                textBox1.Clear();
+                textBox1.Focus();
+                return;
+            }
+
+            int a = 0;
+            int b = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (guess[i] == answer[i])
+                {
+                    a++;
+                }
+                else if (answer.Contains(guess[i]))
+                {
+                    b++;
+                }
+            }
+
+            listBox1.Items.Add($"{guess} : {a}A{b}B");
+
+            if (a == 4)
+            {
+                MessageBox.Show("過關");
+
+                textBox1.Enabled = false;
+                btnCheck.Enabled = false;
+            }
+            btnStart.Enabled = false;
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+        private bool IsValidGuess(string guess)
+        {
+            if (guess.Length != 4)
+                return false;
+
+            if (!guess.All(char.IsDigit))
+                return false;
+
+            if (guess.Distinct().Count() != 4)
+                return false;
+
+            return true;
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"答案是：{answer}");
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+            btnStart.Enabled = true;
         }
     }
 }
